1928323968406da958f15f35c4f2a4a5d9b0a45c
Lucas Lemos <guedeslemos29@gmail.com>
2023-03-12 19:24:27 -0300

    remove `sqlfluff` as a sql linter/formater

--- .config/nvim/lua/lemos/null-ls.lua
+++ .config/nvim/lua/lemos/null-ls.lua
@@ -9,14 +9,8 @@ null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.eslint_d,       -- ESLint suggestion code_actions_eslint_d
     null_ls.builtins.formatting.prettierd,        -- prettier, as a daemon, for ludicrous formatting speed.
-    null_ls.builtins.formatting.sqlfluff.with({
-        extra_args = { "--dialect", "sqlite" },
-    }),
     null_ls.builtins.formatting.shfmt,            -- A shell parser, formatter, and interpreter
     null_ls.builtins.diagnostics.eslint_d,        -- ESLint diagnostics
     null_ls.builtins.diagnostics.actionlint,      -- static checker for GitHub Actions workflow files
-    null_ls.builtins.diagnostics.sqlfluff.with({  -- SQL linter and auto-formatter for Humans
-        extra_args = { "--dialect", "sqlite" },   -- note that you must specify a dialect.
-    }),                                           -- more information on https://docs.sqlfluff.com/en/stable/configuration.html
   }
 })
